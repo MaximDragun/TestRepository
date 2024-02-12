863bf19ed0040ec54afb66863a6b57787281e6ab
Maxim Dragun <mdragun@alfabank.ru>
2024-02-12 19:33:54 +0300

    new

---
+++ src/main/kotlin/com/example/testrepo/TestRepoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testrepo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestRepoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestRepoApplication>(*args)
+}
